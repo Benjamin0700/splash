9ff43d683bb4ca7b1e0e8f8ed5914f3b1276f7cd
benjamin0700 <jalolsoxiddinov777@gmail.com>
2025-05-09 20:06:02 +0500

    done

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server: {
-    https: {
-      key: './localhost-key.pem',  // путь к приватному ключу
-      cert: './localhost.pem',     // путь к сертификату
-    },
-  },
-});
+  base: '/splash/', // GitHub repo nomi
+  plugins: [react()],
+})
